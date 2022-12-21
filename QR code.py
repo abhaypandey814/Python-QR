3827e27db4634f63f1b64b270257f6408c81b1f0
Abhay Pandey <111235794+abhaypandey814@users.noreply.github.com>
2022-12-21 22:26:19 +0530

    Add files via upload

---
+++ QR code.py
@@ -0,0 +1,17 @@
+import qrcode
+import image
+qr = qrcode.QRCode(
+    version=15,
+    box_size=10,
+    border=5
+)
+
+data="https://www.linkedin.com/in/abhay-pandey-2b25b4222"
+
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black",back_color="white")
+img.save("test.png")
+
+
+
